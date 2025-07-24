1e9e86988194751144c1565aee8d362c7dff56d7
KoolKrish18 <nalamkrish1@gmail.com>
2025-07-24 18:16:29 -0400

    feat: untested code for autofill with temp vals

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,26 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
-  },
+	matches: ['<all_urls>'],
+	main() {
+		console.log('Hello content.');
+	},
 });
+
+function getInputField() {
+	const inputs = document.querySelectorAll('input'); //, textarea, select
+	inputs.forEach((input) => {
+		const labelElement = document.querySelector(`label[for="${input.id}"]`);
+		const label = labelElement instanceof HTMLElement ? labelElement.innerText : input.placeholder || '';
+		const value = fillInputField(label);
+		value && (input.value = value);
+	});
+}
+
+function fillInputField(label: string): string | null {
+	label = label.toLowerCase();
+	if (label.includes('name')) return 'John Doe';
+	if (label.includes('email')) return 'john@example.com';
+	if (label.includes('phone') || label.includes('number')) return '1234567890';
+	if (label.includes('address')) return '123 Main St';
+	if (label.includes('date')) return '2025-01-01';
+	return null;
+}
