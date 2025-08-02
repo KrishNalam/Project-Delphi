9e547207197952e823b4dd5c5969f16ec27452ce
KoolKrish18 <nalamkrish1@gmail.com>
2025-08-02 12:47:41 -0400

    feat: created form with inputs for basic fields

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,4 +1,25 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+type UserData = {
+	name: string;
+	email: string;
+	phone: string;
+	address: string;
+	linkedin: string;
+	github: string;
+	portfolio: string;
+};
+
+const defaultUserData: UserData = {
+	name: '',
+	email: '',
+	phone: '',
+	address: '',
+	linkedin: '',
+	github: '',
+	portfolio: '',
+};
 
 function App() {
 	const handleClick = () => {
@@ -8,8 +29,18 @@ function App() {
 			}
 		});
 	};
+
 	return (
 		<>
+			<form>
+				<input placeholder='First Name' type='text' id='fname' />
+				<input placeholder='Last Name' type='text' id='lname' />
+				<input placeholder='Email' type='text' id='email' />
+				<input placeholder='Phone' type='text' id='phone' />
+				<input placeholder='LinkedIn' type='text' id='linkedin' />
+				<input placeholder='GitHub' type='text' id='github' />
+				<input placeholder='Portfolio' type='text' id='portfolio' />
+			</form>
 			<div className='card'>
 				<button onClick={handleClick}>Autofill</button>
 			</div>
