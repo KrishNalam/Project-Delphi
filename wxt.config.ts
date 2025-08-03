f7bfd00cbbba49cd54e6cb6f279659b7b8884155
KoolKrish18 <nalamkrish1@gmail.com>
2025-08-03 11:37:52 -0400

    fix: wxt pages initialization

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	modules: ['@wxt-dev/module-react'],
-	pages: {
-		form: './src/pages/formPage.tsx', // 👈 This is your custom form page
-	},
 	manifest: {
 		permissions: ['storage'],
 	},
