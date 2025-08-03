ac39ef57f967beeb8d446dfaed7e15b490b7257f
KoolKrish18 <nalamkrish1@gmail.com>
2025-08-03 11:36:03 -0400

    chore: allow perms for chrome storage

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+	modules: ['@wxt-dev/module-react'],
+	pages: {
+		form: './src/pages/formPage.tsx', // 👈 This is your custom form page
+	},
+	manifest: {
+		permissions: ['storage'],
+	},
 });
